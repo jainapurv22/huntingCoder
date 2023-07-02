b05e5d0cb61fd133d4af2a76629ba140dfb4783b
Apurv Jain <jainapurv35@gmail.com>
2023-07-02 23:08:16 +0530

    first changes after image addition

---
+++ components/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      This is contact page of us
+    </div>
+  )
+}
+
+export default contact
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-12">
+      <nav className='mb-[40px]'>
+        <ul className="flex justify-center">
+          <li className='mx-[15px]'>
+            <Link href={'/'}>Home</Link>
+          </li>
+          <li className='mx-[15px]'>
+            <Link href={'/about'}>About</Link>
+          </li>
+          <li className='mx-[15px]'>
+            <Link href={'/blog'}>Blog</Link>
+          </li>
+          <li className='mx-[15px]'>
+            <Link href={'/contact'}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="z-10 flex w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <h1 className="fixed left-0 top-0 font-bold text-[30px] w-full justify-center  bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:from-inherit lg:static lg:w-auto   lg:p-4 ">
+          Hunting Coder
+        </h1>
+        
+      </div>
+      <Image alt='' src={'/homeImg1.avif'} width={148} height={128} className='rounded-lg mt-[15px]'></Image>
+      <div className="z-10 flex mt-[40px] w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0  w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          A blog for hunting coders by a hunting coder
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -28,21 +51,25 @@ export default function Home() {
               priority
             />
           </a>
+        </div> */}
+      </div>
+      <div className='mt-[40px]'>
+        <h3 className='text-[38px]'>Popular Blogs</h3>
+        <div className='my-[20px] text-[16px]'> 
+          <h2 className='font-bold text-[28px]'>How to learn javascript in 2022?</h2>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+        <div className='my-[20px] text-[16px]'> 
+          <h2 className='font-bold text-[28px]'>How to learn javascript in 2022?</h2>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+        <div className='my-[20px] text-[16px]'> 
+          <h2 className='font-bold text-[28px]'>How to learn javascript in 2022?</h2>
+          <p>Javascript is the language used to design logic for the web</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -118,7 +145,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
