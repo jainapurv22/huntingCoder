1c0475cf9a2b1032e487a01100ba6369e3fee82e
Apurv Jain <jainapurv35@gmail.com>
2023-07-04 01:07:33 +0530

    ssr

--- pages/blog.js
+++ pages/blog.js
@@ -1,18 +1,11 @@
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 
-const blog = () => {
-  const [blogs, setblogs] = useState()
-  useEffect(() => {
-    // console.log('useEffect is running')
-    fetch('http://localhost:3000/api/blogs').then((a)=>{
-      return a.json();})
-      .then((parsed)=>{
-          // console.log(parsed);
-          setblogs(parsed);
-      })
-  }, [])
-  // console.log(blogs)
+const blog = (props) => {
+  const [blogs, setblogs] = useState(props.allBlogs)
+  // useEffect(() => {
+    
+  // }, [])
   return <div className='mt-[40px] flex items-center flex-col'>
     <h3 className='text-[38px]'>Latest Blogs</h3>
     {
@@ -26,23 +19,15 @@ const blog = () => {
     })
   }
   </div>
-  
-  // return (
-  //   <div>
-  //     <div className='mt-[40px] flex items-center flex-col'>
-  //       <h3 className='text-[38px]'>Latest Blogs</h3>
-        
-  //       <div className='my-[20px] text-[16px]'> 
-  //         <Link href={'/blogpost/How to learn javascript in 2022'} className='font-bold text-[28px]'>How to learn javascript in 2022?</Link>
-  //         <p>Javascript is the language used to design logic for the web</p>
-  //       </div>
-  //       <div className='my-[20px] text-[16px]'> 
-  //         <Link href={'/blogpost/How to learn javascript in 2022'} className='font-bold text-[28px]'>How to learn javascript in 2022?</Link>
-  //         <p>Javascript is the language used to design logic for the web</p>
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
+}
+
+export async function getServerSideProps() {
+  let data = await fetch('http://localhost:3000/api/blogs');
+  let allBlogs = await data.json();
+
+  return {
+    props: {allBlogs},
+  }
 }
 
 export default blog
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Router, { useRouter } from 'next/router'
 
-const Slug = () => {
-  const router=useRouter();
-  const {slug} = router.query;
+const Slug = (props) => {
+  const [blog, setBlog] = useState(props.blogitem);
+  
   return (
     <div className='flex items-center flex-col'>
-      <h2 className='text-[25px] mb-[15px] font-bold'>Title of the blogpost is {slug}</h2>
-      <p className='px-[24rem]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore architecto culpa quas placeat ut possimus sed nostrum quisquam, quod nemo, blanditiis reiciendis, doloribus porro! Maiores.</p>
+      <h2 className='text-[25px] mb-[15px] font-bold'>{blog&&blog.title}</h2>
+      <p className='px-[24rem]'>{blog&&blog.description}</p>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+    
+    const {slug} = context.query;
+    let data = await fetch(`http://localhost:3000/api/getBlog?slug=${slug}`)
+    let blogitem = await data.json();
+
+  return {
+    props: {blogitem},
+  }
 }
 
 export default Slug
